79ecd0354c985af2bd518ac357c85f39c7e724c3
Zeydel <mads_seydel@yahoo.dk>
2017-11-03 11:18:16 +0100

    meget bedre raflebæger

--- 15_del2/src/CDIO2/spilSpil.java
+++ 15_del2/src/CDIO2/spilSpil.java
@@ -5,15 +5,10 @@ public class spilSpil {
 	
 	public static void main (String[] args) {
 		
+		Raflebæger cup = new Raflebæger(5);
 
 		Felt.BoardController();
-		
-		
-		
-		Raflebæger.rollCup(5);
-		Raflebæger.getSum();
-		Terning.setAntalØjne(9001);
-		System.out.println(Terning.getAntalØjne());
+
 	
 		
 	
