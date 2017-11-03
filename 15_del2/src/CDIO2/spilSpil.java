940a55b93b5951bc77b5f3c96aa68b4efda84764
Alex <Alex@192.168.0.18>
2017-11-03 12:15:53 +0100

    ingen overskydende felter. ingen billeder. det spiller! ;)

--- 15_del2/src/CDIO2/spilSpil.java
+++ 15_del2/src/CDIO2/spilSpil.java
@@ -1,17 +1,43 @@
 package CDIO2;
 
+import desktop_fields.Empty;
+import desktop_fields.Field;
+import desktop_fields.Street;
+import desktop_resources.GUI;
 
 public class spilSpil {
 	
 	public static void main (String[] args) {
-		
-		Raflebæger cup = new Raflebæger(5);
+		new spilSpil().spil();		
+	}
 
-		Felt.BoardController();
-
-	
+	private void spil() {
+		initializeGUI();
+		GUI.showMessage("velkommen");
+		boolean winner = false;
 		
-	
+		while(!winner) {
+			//TODO gameLoop;
+			
+		}
+		
+	}
+
+	private void initializeGUI() {
+		// TODO Create Fields
+		Field [] fields = new Field[40];
+		// Navngiv felter der skal bruges
+		for (int i = 0; i < Oversæt.file1().size(); i++) {
+		fields[i] = new Street.Builder().setTitle(Felt.BoardController1(i)).setDescription(Felt.BoardController3(i)).setSubText(Felt.BoardController2(i)).build();
+		}
+		// Lav tomme felter som ikke skal bruges
+		for (int i = Oversæt.file1().size(); i < fields.length; i++) {
+		fields[i] = new Empty.Builder().build();
+		}
+		
+		GUI.create(fields);
+
+		//TODO send Fields to GUI
 		
 	}
 }
