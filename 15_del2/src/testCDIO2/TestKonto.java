0ebcdbadad76d0be5833fb08a00474a3ab412304
oliveruschultz <oliveruldallschultz@gmail.com>
2017-11-07 17:41:26 +0100

    Test for konto og spiller oprettet

---
+++ 15_del2/src/testCDIO2/TestKonto.java
@@ -0,0 +1,39 @@
+package testCDIO2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestKonto {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetVærdi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetVærdi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTilføjVærdi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHævVærdi() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ 15_del2/src/testCDIO2/TestSpiller.java
@@ -0,0 +1,39 @@
+package testCDIO2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSpiller {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetNavn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNavn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPosition() {
+		fail("Not yet implemented");
+	}
+
+}
