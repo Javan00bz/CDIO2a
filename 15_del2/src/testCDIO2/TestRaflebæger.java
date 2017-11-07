0b9ec658e6700c13d17df2bb52c86e50589e26c8
Zeydel <mads_seydel@yahoo.dk>
2017-11-07 18:53:36 +0100

    test raflebæger

---
+++ 15_del2/src/testCDIO2/TestRaflebæger.java
@@ -0,0 +1,31 @@
+package testCDIO2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import CDIO2.Raflebæger;
+import CDIO2.Terning;
+
+public class TestRaflebæger {
+	Raflebæger cup = new Raflebæger(2);
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetSum() {
+		Terning[] dice;
+		dice[0] = ;
+		
+		cup.setTerninger(dice);
+	}
+
+}
