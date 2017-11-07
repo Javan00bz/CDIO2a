7fec58900340c9599c2bca1a5ef13f4ee8edb3d8
oliveruschultz <oliveruldallschultz@gmail.com>
2017-11-07 18:35:20 +0100

    Randomnesstest for terning

--- 15_del2/src/testCDIO2/TestTerning.java
+++ 15_del2/src/testCDIO2/TestTerning.java
@@ -19,10 +19,6 @@ public class TestTerning {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testTerning() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testRulTerning() {
@@ -46,6 +42,34 @@ public class TestTerning {
 	public void testSetAntalØjne() {
 		ter1.setAntalØjne(6);
 		assertEquals(6, ter1.getAntalØjne());
+	}
+	
+	@Test
+	public void TestRandomnessDice()
+	{
+		int amount6 = 0;
+		System.out.println("");
+		for(int o = 0 ; o<1000;o++) {
+			int a1 = ter1.rulTerning();
+			int actual = a1;
+			if (actual == 6)
+				amount6++;
+		}
+		System.out.println(amount6);
+		assertTrue("Error, random is too high", 185 >= amount6);
+		assertTrue("Error, random is too low",  148  <= amount6);
+	}
+	
+		
+	public void TestResponseTid() {
+		long l = System.currentTimeMillis();
+		System.out.println(System.currentTimeMillis());
+		for(int o = 0 ; o<1000;o++) {
+			int actual = ter1.rulTerning();
+			}
+		long k = System.currentTimeMillis();
+		assertTrue(k-l<333000);
+		System.out.println(k-l);
 	}
 
 }
