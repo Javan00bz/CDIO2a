9506370e0ee12b6e06006dd077d23b55f556706b
Zeydel <mads_seydel@yahoo.dk>
2017-11-09 18:10:15 +0100

    To nye metoder til main + omstrukturering

--- 15_del2/src/CDIO2/Spil.java
+++ 15_del2/src/CDIO2/Spil.java
@@ -9,6 +9,10 @@ import desktop_resources.GUI;
 
 public class Spil {
 
+	String besked = null;
+	boolean winner = false;
+	boolean yourturn = true;
+	Raflebæger cup = new Raflebæger(2);
 	public static void main (String[] args) {
 		new Spil().spilSpil();	
 	}
@@ -25,191 +29,141 @@ public class Spil {
 			GUI.showMessage(StandardBeskeder (2));
 			Spiller2Navn = GUI.getUserString(StandardBeskeder (4));
 		}
-		 
+
 		Spiller Spiller2 = new Spiller(Spiller2Navn, 6, Konto2 );
 		GUI.addPlayer(Spiller1.getNavn(), Konto1.getVærdi());
 		GUI.addPlayer(Spiller2.getNavn(), Konto2.getVærdi());
-		Raflebæger cup = new Raflebæger(2);
-		String besked = null;
-		boolean winner = false;
-		boolean yourturn = true;
-	
+
+
+
+
 		//hele spillets loop
 		while(!winner) {
-			
-			yourturn = true;
-			while(yourturn == true && winner == false) {
-				GUI.getUserButtonPressed(besked, Spiller1.getNavn() + StandardBeskeder(5));
-				cup.rulRaflebæger();
-				GUI.setDice(cup.getTerninger()[0].getAntalØjne(), cup.getTerninger()[1].getAntalØjne());
-				Spiller1.setPosition(cup.getSum() - 1);
-				GUI.removeAllCars(Spiller1.getNavn());
-				GUI.setCar(Spiller1.getPosition(), Spiller1.getNavn());
-				
-				//Finder ud af hvor mange point spilleren skal givet eller fratages.
-				switch (Spiller1.getPosition()) {
-				case 1: Konto1.tilføjVærdi(250);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 2: Konto1.hævVærdi(100);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 3: Konto1.tilføjVærdi(100);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 4: Konto1.hævVærdi(20);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 5: Konto1.tilføjVærdi(180);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 6: Konto1.tilføjVærdi(0);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 7: Konto1.hævVærdi(70);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 8: Konto1.tilføjVærdi(60);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 9: Konto1.hævVærdi(80);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 10: Konto1.hævVærdi(50);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				case 11: Konto1.tilføjVærdi(650);
-				besked = hentFeltBeskrivelse(Spiller1.getPosition()-1);
-				break;
-				default: besked = StandardBeskeder (6);
-				}
-				
-					
-				// Sætter spillerens balance of finder ud af om spilleren har vundet og finder ud af om spilleren på slå igen.
-				GUI.setBalance(Spiller1.getNavn(), Konto1.getVærdi());
-				if (Konto1.getVærdi() >= 3000)
-					winner = true;
-				if (Spiller1.getPosition() == 9)
-					yourturn = true;
-				else yourturn = false;
-			}
-			yourturn = true;
-			while(yourturn == true && winner == false ) {
-				GUI.getUserButtonPressed(besked, Spiller2.getNavn() + StandardBeskeder(5));
-				cup.rulRaflebæger();
-				GUI.setDice(cup.getTerninger()[0].getAntalØjne(), cup.getTerninger()[1].getAntalØjne());
-				Spiller2.setPosition(cup.getSum() - 1);
-				GUI.removeAllCars(Spiller2.getNavn());
-				GUI.setCar(Spiller2.getPosition(), Spiller2.getNavn());
-
-				switch (Spiller2.getPosition()) {
-				case 1: Konto2.tilføjVærdi(250);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 2: Konto2.hævVærdi(100);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 3: Konto2.tilføjVærdi(100);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 4: Konto2.hævVærdi(20);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 5: Konto2.tilføjVærdi(180);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 6: Konto2.tilføjVærdi(0);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 7: Konto2.hævVærdi(70);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 8: Konto2.tilføjVærdi(60);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 9: Konto2.hævVærdi(80);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 10: Konto2.hævVærdi(50);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				case 11: Konto2.tilføjVærdi(650);
-				besked = hentFeltBeskrivelse(Spiller2.getPosition()-1);
-				break;
-				default: besked = StandardBeskeder (6);
-				}
-
-				GUI.setBalance(Spiller2.getNavn(), Konto2.getVærdi());
-				
-				if (Konto2.getVærdi() >= 3000)
-					winner = true;
-				if (Spiller2.getPosition() == 9)
-					yourturn = true;
-				else yourturn = false;
-
-			}
+			tur(Spiller1);
+			tur(Spiller2);
 		}
-		if (Konto1.getVærdi() > Konto2.getVærdi())
-			GUI.showMessage(Spiller1.getNavn() +  StandardBeskeder(7));
-		else GUI.showMessage(Spiller2.getNavn() + StandardBeskeder(7));
-		}
-
-		private void initializeGUI() {
-			Field [] fields = new Field[40];
-			// Navngiv felter der skal bruges
-			for (int i = 0; i < Oversæt.file1().size(); i++) {
-				fields[i] = new Street.Builder()
-						.setTitle(hentFeltOverskrift(i)).setDescription(hentFeltBeskrivelse(i))
-						.setSubText(hentFeltVærdi(i))
-						.build();
-			}
-			// Lav tomme felter som ikke skal bruges
-			for (int i = Oversæt.file1().size(); i < fields.length; i++) {
-				fields[i] = new Empty.Builder().build();
-			}
-
-			GUI.create(fields);
-
-
-		}
-
-		public static String hentFeltOverskrift(int i){
-
-			ArrayList<String> lines1 = new ArrayList<String>();
-			lines1 = Oversæt.file1();
-			String[] li1 = new String[lines1.size()];
-			li1 = lines1.toArray(li1);
-
-			return li1[i];
-
-		}
-
-		public static String hentFeltVærdi(int i){
-
-			ArrayList<String> lines2 = new ArrayList<String>();
-			lines2 = Oversæt.file2();
-			String[] li2 = new String[lines2.size()];
-			li2 = lines2.toArray(li2);
-			return li2[i];
-		}
-
-
-		public static String hentFeltBeskrivelse(int i){
-
-			ArrayList<String> lines3 = new ArrayList<String>();
-			lines3 = Oversæt.file3();
-			String[] li3 = new String[lines3.size()];
-			li3 = lines3.toArray(li3);
-			return li3[i];
-		}
-		
-		public static String StandardBeskeder(int i){
-
-			ArrayList<String> lines3 = new ArrayList<String>();
-			lines3 = Oversæt.file4();
-			String[] li3 = new String[lines3.size()];
-			li3 = lines3.toArray(li3);
-			return li3[i];
-		}
-		
+		endGame(Spiller1, Spiller2);
 	}
+
+	private void initializeGUI() {
+		Field [] fields = new Field[40];
+		// Navngiv felter der skal bruges
+		for (int i = 0; i < Oversæt.file1().size(); i++) {
+			fields[i] = new Street.Builder()
+					.setTitle(hentFeltOverskrift(i)).setDescription(hentFeltBeskrivelse(i))
+					.setSubText(hentFeltVærdi(i))
+					.build();
+		}
+		// Lav tomme felter som ikke skal bruges
+		for (int i = Oversæt.file1().size(); i < fields.length; i++) {
+			fields[i] = new Empty.Builder().build();
+		}
+
+		GUI.create(fields);
+
+
+	}
+
+	public static String hentFeltOverskrift(int i){
+
+		ArrayList<String> lines1 = new ArrayList<String>();
+		lines1 = Oversæt.file1();
+		String[] li1 = new String[lines1.size()];
+		li1 = lines1.toArray(li1);
+
+		return li1[i];
+
+	}
+
+	public static String hentFeltVærdi(int i){
+
+		ArrayList<String> lines2 = new ArrayList<String>();
+		lines2 = Oversæt.file2();
+		String[] li2 = new String[lines2.size()];
+		li2 = lines2.toArray(li2);
+		return li2[i];
+	}
+
+
+	public static String hentFeltBeskrivelse(int i){
+
+		ArrayList<String> lines3 = new ArrayList<String>();
+		lines3 = Oversæt.file3();
+		String[] li3 = new String[lines3.size()];
+		li3 = lines3.toArray(li3);
+		return li3[i];
+	}
+
+	public static String StandardBeskeder(int i){
+
+		ArrayList<String> lines3 = new ArrayList<String>();
+		lines3 = Oversæt.file4();
+		String[] li3 = new String[lines3.size()];
+		li3 = lines3.toArray(li3);
+		return li3[i];
+	}
+	public void tur(Spiller CurrentPlayer) {
+		yourturn = true;
+		while(yourturn == true && winner == false) {
+			GUI.getUserButtonPressed(besked, CurrentPlayer.getNavn() + StandardBeskeder(5));
+			cup.rulRaflebæger();
+			GUI.setDice(cup.getTerninger()[0].getAntalØjne(), cup.getTerninger()[1].getAntalØjne());
+			CurrentPlayer.setPosition(cup.getSum() - 1);
+			GUI.removeAllCars(CurrentPlayer.getNavn());
+			GUI.setCar(CurrentPlayer.getPosition(), CurrentPlayer.getNavn());
+
+			//Finder ud af hvor mange point spilleren skal givet eller fratages.
+			switch (CurrentPlayer.getPosition()) {
+			case 1: CurrentPlayer.Konto.tilføjVærdi(250);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 2: CurrentPlayer.Konto.hævVærdi(100);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 3: CurrentPlayer.Konto.tilføjVærdi(100);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 4: CurrentPlayer.Konto.hævVærdi(20);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 5: CurrentPlayer.Konto.tilføjVærdi(180);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 6: CurrentPlayer.Konto.tilføjVærdi(0);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 7: CurrentPlayer.Konto.hævVærdi(70);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 8: CurrentPlayer.Konto.tilføjVærdi(60);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 9: CurrentPlayer.Konto.hævVærdi(80);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 10: CurrentPlayer.Konto.hævVærdi(50);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			case 11: CurrentPlayer.Konto.tilføjVærdi(650);
+			besked = hentFeltBeskrivelse(CurrentPlayer.getPosition()-1);
+			break;
+			default: besked = StandardBeskeder (6);
+			}
+
+
+
+			GUI.setBalance(CurrentPlayer.getNavn(), CurrentPlayer.Konto.getVærdi());
+			if (CurrentPlayer.Konto.getVærdi() >= 3000)
+				winner = true;
+			if (CurrentPlayer.getPosition() == 9)
+				yourturn = true;
+			else yourturn = false;
+		}
+	}
+	public void endGame(Spiller FørsteSpiller, Spiller AndenSpiller) {
+		if (FørsteSpiller.Konto.getVærdi() > AndenSpiller.Konto.getVærdi())
+			GUI.showMessage(FørsteSpiller.getNavn() +  StandardBeskeder(7));
+		else GUI.showMessage(AndenSpiller.getNavn() + StandardBeskeder(7));
+	}
+
+}
