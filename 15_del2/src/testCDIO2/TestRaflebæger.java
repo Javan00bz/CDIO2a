8456193bf11c422f47844a57aaee7aa18d68b674
Zeydel <mads_seydel@yahoo.dk>
2017-11-10 08:13:20 +0100

    TestSum raflebæger færdig

--- 15_del2/src/testCDIO2/TestRaflebæger.java
+++ 15_del2/src/testCDIO2/TestRaflebæger.java
@@ -11,6 +11,10 @@ import CDIO2.Terning;
 
 public class TestRaflebæger {
 	Raflebæger cup = new Raflebæger(2);
+	Terning ter1 = new Terning(6);
+	Terning ter2 = new Terning(6);
+	Terning ter3 = new Terning(6);
+	
 
 	@Before
 	public void setUp() throws Exception {
@@ -22,8 +26,15 @@ public class TestRaflebæger {
 
 	@Test
 	public void testGetSum() {
-		Terning[] dice;
-		dice[0] = ;
+		int sum = 0;
+		ter1.setAntalØjne(1);
+		ter2.setAntalØjne(6);
+		ter3.setAntalØjne(4);
+		Terning[] dice = {ter1, ter2, ter3};
+		cup.setTerninger(dice);
+		for (int i = 0; i<3; i++)
+			sum = sum + cup.getTerninger()[i].getAntalØjne();
+		assertEquals(11, sum);
 		
 		cup.setTerninger(dice);
 	}
